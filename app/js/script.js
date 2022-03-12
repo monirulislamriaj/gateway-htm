85447ca9dc6db42e724ae8a574c3d185a08795a5
monirulislamriaj <riaj1132@gmail.com>
2022-03-12 09:22:38 +0600

    inital commit

---
+++ app/js/script.js
@@ -0,0 +1,29 @@
+// Passive event listeners
+jQuery.event.special.touchstart = {
+	setup: function (_, ns, handle) {
+		'use strict';
+    this.addEventListener('touchstart', handle, {
+      passive: !ns.includes('noPreventDefault')
+    });
+  }
+};
+jQuery.event.special.touchmove = {
+	setup: function (_, ns, handle) {
+		'use strict';
+    this.addEventListener('touchmove', handle, {
+      passive: !ns.includes('noPreventDefault')
+    });
+  }
+};
+
+// Preloader js
+$(window).on('load', function () {
+  'use strict';
+  $('.preloader').fadeOut(0);
+});
+
+// on ready state
+$(document).ready(function () {
+  'use strict';
+
+});
